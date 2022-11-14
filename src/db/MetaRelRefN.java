5c40b433376e66b059a56341f7a85518104ac294
c <c@c>
2022-11-14 01:34:01 +0100

    .

--- src/db/MetaRelRefN.java
+++ src/db/MetaRelRefN.java
@@ -4,35 +4,36 @@ import java.util.Set;
 
 // represents the relation table
 final class MetaRelRefN {
-	final Class<? extends DbObject> fromCls;
-	final Class<? extends DbObject> toCls;
+//	final Class<? extends DbObject> fromCls;
+//	final Class<? extends DbObject> toCls;
 	final String tableName; // the table name for this association NN table
+	final String fromTableName; // the table name of the source
 	final String fromColName; // column name where id referencing source of relation id
-	final String toColName; // column name where id to referencing target
 	final String toTableName; // table name of "to" class
+	final String toColName; // column name where id to referencing target
 
-	public MetaRelRefN(final Class<? extends DbObject> fromCls, final String relName,
-			final Class<? extends DbObject> toCls) {
-		this.fromCls = fromCls;
-		this.toCls = toCls;
-		this.fromColName=Db.tableNameForJavaClass(fromCls);
-		this.toTableName=Db.tableNameForJavaClass(toCls);
-		this.toColName=Db.tableNameForJavaClass(toCls);
+	MetaRelRefN(final Class<? extends DbObject> fromCls, final String relName, final Class<? extends DbObject> toCls) {
+//		this.fromCls = fromCls;
+//		this.toCls = toCls;
+		this.fromTableName = Db.tableNameForJavaClass(fromCls);
+		this.fromColName = Db.tableNameForJavaClass(fromCls);
+		this.toTableName = Db.tableNameForJavaClass(toCls);
+		this.toColName = Db.tableNameForJavaClass(toCls);
 		tableName = new StringBuilder(256).append(Db.tableNameForJavaClass(fromCls)).append('_').append(relName)
 				.toString();
 	}
 
-	public void sql_createTable(final StringBuilder sb) {
-		sb.append("create table ").append(tableName).append('(').append(Db.tableNameForJavaClass(fromCls))
-				.append(" int,").append(Db.tableNameForJavaClass(toCls)).append(" int)");
+	void sql_createTable(final StringBuilder sb) {
+		sb.append("create table ").append(tableName).append('(').append(fromTableName).append(" int,")
+				.append(toTableName).append(" int)");
 	}
 
-	public void sql_addToTable(final StringBuilder sb, DbObject from, DbObject to) {
+	void sql_addToTable(final StringBuilder sb, DbObject from, DbObject to) {
 		sb.append("insert into ").append(tableName).append(" values(").append(from.getId()).append(',')
 				.append(to.getId()).append(')').toString();
 	}
 
-	public boolean tableIsIn(Set<String> tblNames) {
+	boolean tableIsIn(Set<String> tblNames) {
 		return tblNames.contains(tableName);
 	}
 }
