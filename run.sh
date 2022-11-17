599c254c1a0d61a14afbd443000c0f766d72ac27
c <c@c>
2022-11-17 15:31:04 +0100

    .

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+DIR=$(dirname "$0")
+cd $DIR &&
+
+cmd="java -cp bin`ls $DIR/lib|while read f;do echo -n :;echo -n $DIR/lib/$f;done` main.Main $*" &&
+echo \> $cmd;$cmd
+
+
+#java -XX:+UnlockDiagnosticVMOptions -XX:+PrintInlining -cp bin:../b/bin b.b
