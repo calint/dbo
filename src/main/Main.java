9a120f171d71d9cb7c8f9b000a4909ae344fdc2f
c <c@c>
2022-11-20 04:03:49 +0100

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,6 @@ import java.util.List;
 import db.Db;
 import db.DbObject;
 import db.DbTransaction;
-import db.Limit;
-import db.Query;
 
 public class Main {
 	public static final void main(String[] args) throws Throwable {
@@ -34,7 +32,7 @@ public class Main {
 	}
 }
 
-// full text search books
+// delete all books (cascading delete of aggregated DataText)
 class ReqThread extends Thread {
 	@Override
 	public void run() {
@@ -45,19 +43,12 @@ class ReqThread extends Thread {
 			throw new RuntimeException(t1);
 		}
 		try {
-// ---- - - --- -- -------- -- - - -- -- -- -- - -- --- - --- --- - -- -- -- --
-			List<DbObject> ls;
-			Query q;
-			Limit l;
-			
-			q = new Query(Book.ft, "+guide +ultimate -training");
-			l = new Limit(0,25);
-			ls = tn.get(Book.class, q, null, l);
-			System.out.println("results: " + ls.size());
+//---- - - --- -- -------- -- - - -- -- -- -- - -- --- - --- --- - -- -- -- --
+			List<DbObject> ls = tn.get(Book.class, null, null, null);
 			for (final DbObject o : ls) {
-				System.out.println(o);
+				tn.delete(o);
 			}
-// ---- - - --- -- -------- -- - - -- -- -- -- - -- --- - --- --- - -- -- -- --
+//---- - - --- -- -------- -- - - -- -- -- -- - -- --- - --- --- - -- -- -- --
 			tn.commit();
 		} catch (Throwable t1) {
 			try {
@@ -71,6 +62,45 @@ class ReqThread extends Thread {
 		}
 	}
 }
+
+//// full text search books
+//class ReqThread extends Thread {
+//	@Override
+//	public void run() {
+//		DbTransaction tn;
+//		try {
+//			tn = Db.initCurrentTransaction();
+//		} catch (Throwable t1) {
+//			throw new RuntimeException(t1);
+//		}
+//		try {
+//// ---- - - --- -- -------- -- - - -- -- -- -- - -- --- - --- --- - -- -- -- --
+//			List<DbObject> ls;
+//			Query q;
+//			Limit l;
+//			
+//			q = new Query(Book.ft, "+guide +ultimate -training");
+//			l = new Limit(0,25);
+//			ls = tn.get(Book.class, q, null, l);
+//			System.out.println("results: " + ls.size());
+//			for (final DbObject o : ls) {
+//				System.out.println(o);
+//			}
+//// ---- - - --- -- -------- -- - - -- -- -- -- - -- --- - --- --- - -- -- -- --
+//			tn.commit();
+//		} catch (Throwable t1) {
+//			try {
+//				tn.rollback();
+//			} catch (Throwable t2) {
+//				t2.printStackTrace();
+//			}
+//			throw new RuntimeException(t1);
+//		} finally {
+//			Db.deinitCurrentTransaction();
+//		}
+//	}
+//}
+
 // prints column types
 //class ReqThread extends Thread {
 //	@Override
