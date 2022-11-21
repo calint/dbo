e24aa5cac89e07962c4c955ef9277f69648f23f4
c <c@c>
2022-11-21 13:25:57 +0100

    .

--- src/db/DbObject.java
+++ src/db/DbObject.java
@@ -10,103 +10,11 @@ public abstract class DbObject {
 	final HashMap<DbField, Object> fieldValues = new HashMap<DbField, Object>();
 	HashSet<DbField> dirtyFields;
 
-	private void ensureDirtyFieldsIsCreated() {
-		if (dirtyFields != null)
-			return;
-		dirtyFields = new HashSet<DbField>();
+	private HashSet<DbField> getCreatedDirtyFields() {
+		if (dirtyFields == null)
+			dirtyFields = new HashSet<DbField>();
+		return dirtyFields;
 	}
-
-//	final void createInDb() throws Throwable {// ? move code to DbTransaction?
-//		final DbTransaction t = Db.currentTransaction();
-//		final Statement s = t.stmt;
-//		final StringBuilder sbSql = new StringBuilder(256);
-////		sbSql.append("insert into ").append(Db.tableNameForJavaClass(getClass()));
-////		final StringBuilder sbFields = new StringBuilder(256);
-////		final StringBuilder sbValues = new StringBuilder(256);
-////		if (dirtyFields != null) {
-////			for (final DbField f : dirtyFields) {
-////				f.sql_columnName(sbFields);
-////				sbFields.append(',');
-////				f.sql_updateValue(sbValues, this);
-////				sbValues.append(',');
-////			}
-////			dirtyFields.clear();
-////		}
-////		if (sbFields.length() > 0) {
-////			sbSql.append('(');
-////			sbFields.setLength(sbFields.length() - 1);
-////			sbSql.append(sbFields);
-////			sbValues.setLength(sbValues.length() - 1);
-////			sbSql.append(") values(").append(sbValues).append(")");
-////		} else
-////			sbSql.append(" values()");
-//		sbSql.append("insert into ").append(Db.tableNameForJavaClass(getClass())).append(" values()");
-//
-//		final String sql = sbSql.toString();
-//		Db.log(sql);
-//		s.execute(sql, Statement.RETURN_GENERATED_KEYS);
-//		final ResultSet rs = s.getGeneratedKeys();
-//		if (rs.next()) {
-//			final int id = rs.getInt(1);
-//			setId(id);
-//		} else
-//			throw new RuntimeException("no generated id");
-//		rs.close();
-//
-//		// init default values
-//		final DbClass dbcls = Db.instance().dbClassForJavaClass(getClass());
-//		for (final DbField f : dbcls.allFields) {// ? allFields
-//			f.putDefaultValue(fieldValues);
-//		}
-//	}
-
-//	final void updateDb() throws Throwable {// ? move code to DbTransaction?
-//		final DbTransaction t = Db.currentTransaction();
-//		final StringBuilder sb = new StringBuilder(256);
-//		sb.append("update ").append(Db.tableNameForJavaClass(getClass())).append(" set ");
-//		for (final DbField f : dirtyFields) {
-//			sb.append(f.columnName).append('=');
-//			f.sql_updateValue(sb, this);
-//			sb.append(',');
-//		}
-//		sb.setLength(sb.length() - 1);
-//		sb.append(" where id=").append(id());
-//		final String sql = sb.toString();
-//		Db.log(sql);
-//		t.stmt.execute(sql);
-//		dirtyFields.clear();
-//	}
-
-//	final void deleteFromDb() {// ? move code to DbTransaction?
-//		final DbClass dbcls = Db.instance().dbClassForJavaClass(getClass());
-//		for (final DbRelation r : dbcls.allRelations) {
-//			r.cascadeDelete(this);
-//		}
-//
-//		// delete orphans
-//		for (final RelRefN r : dbcls.referingRefN) {
-//			r.deleteReferencesTo(id());
-//		}
-//
-//		// delete this
-//		final StringBuilder sb = new StringBuilder(256);
-//		sb.append("delete from ").append(dbcls.tableName).append(" where id=").append(id());
-//		final String sql = sb.toString();
-//		Db.log(sql);
-//		final DbTransaction tn = Db.currentTransaction();
-//		try {
-//			tn.stmt.execute(sql);
-//		} catch (Throwable t) {
-//			throw new RuntimeException(t);
-//		}
-//		tn.dirtyObjects.remove(this);
-//		if (tn.cache_enabled)
-//			tn.cache.remove(this);
-//	}
-
-//	final void setId(int v) {
-//		fieldValues.put(id, v);
-//	}
 
 	/** alias for getId() */
 	final public int id() {
@@ -151,72 +59,54 @@ public abstract class DbObject {
 
 	final public void set(DbField field, String value) {
 		fieldValues.put(field, value);
-		ensureDirtyFieldsIsCreated();
-		dirtyFields.add(field);
+		getCreatedDirtyFields().add(field);
 		Db.currentTransaction().dirtyObjects.add(this);
 	}
 
 	final public void set(DbField field, int value) {
 		fieldValues.put(field, value);
-		ensureDirtyFieldsIsCreated();
-		dirtyFields.add(field);
+		getCreatedDirtyFields().add(field);
 		Db.currentTransaction().dirtyObjects.add(this);
 	}
 
 	final public void set(DbField field, long value) {
 		fieldValues.put(field, value);
-		ensureDirtyFieldsIsCreated();
-		dirtyFields.add(field);
+		getCreatedDirtyFields().add(field);
 		Db.currentTransaction().dirtyObjects.add(this);
 	}
 
 	final public void set(DbField field, Timestamp value) {
 		fieldValues.put(field, value);
-		ensureDirtyFieldsIsCreated();
-		dirtyFields.add(field);
+		getCreatedDirtyFields().add(field);
 		Db.currentTransaction().dirtyObjects.add(this);
 	}
 
 	final public void set(DbField field, byte[] value) {
 		fieldValues.put(field, value);
-		ensureDirtyFieldsIsCreated();
-		dirtyFields.add(field);
+		getCreatedDirtyFields().add(field);
 		Db.currentTransaction().dirtyObjects.add(this);
 	}
 
 	final public void set(DbField field, float value) {
 		fieldValues.put(field, value);
-		ensureDirtyFieldsIsCreated();
-		dirtyFields.add(field);
+		getCreatedDirtyFields().add(field);
 		Db.currentTransaction().dirtyObjects.add(this);
 	}
 
 	final public void set(DbField field, double value) {
 		fieldValues.put(field, value);
-		ensureDirtyFieldsIsCreated();
-		dirtyFields.add(field);
+		getCreatedDirtyFields().add(field);
 		Db.currentTransaction().dirtyObjects.add(this);
 	}
 
 	final public void set(DbField field, boolean value) {
 		fieldValues.put(field, value);
-		ensureDirtyFieldsIsCreated();
-		dirtyFields.add(field);
+		getCreatedDirtyFields().add(field);
 		Db.currentTransaction().dirtyObjects.add(this);
 	}
-
-//	final void readResultSet(DbClass cls, ResultSet rs) throws Throwable {// ? move code to DbTransaction?
-//		int i = 1;
-//		for (final DbField f : cls.allFields) {
-//			final Object v = rs.getObject(i);
-//			fieldValues.put(f, v);
-//			i++;
-//		}
-//	}
 
 	@Override
 	public String toString() {
 		return new StringBuilder(getClass().getName()).append(" ").append(fieldValues.toString()).toString();
 	}
-
 }
