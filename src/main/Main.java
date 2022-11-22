cbe238ac6fe216b69c00b275df58914323216100
c <c@c>
2022-11-22 04:50:06 +0100

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import db.Db;
+import db.DbObject;
 import db.test.Book;
 import db.test.DataBinary;
 import db.test.DataText;
@@ -10,13 +11,20 @@ import db.test.TestCase;
 import db.test.User;
 import db.test.test1;
 
-public class Main {
+public final class Main {
 	private static void run(Class<? extends TestCase> cls) throws Throwable {
 		final TestCase r = cls.getConstructor().newInstance();
 		final Thread t = new Thread(r);
 		t.start();
 		t.join();
 	}
+
+//	@SuppressWarnings("unchecked")
+//	public static Class<? extends DbObject>[] getClasses() {
+//		final Class<?>[] a = new Class<?>[] { User.class, File.class, DataBinary.class, DataText.class, Book.class,
+//				Game.class };
+//		return (Class<? extends DbObject>[]) a;
+//	}
 
 	public static final void main(String[] args) throws Throwable {
 		Db.initInstance();
@@ -27,13 +35,17 @@ public class Main {
 		db.register(DataText.class);
 		db.register(Book.class);
 		db.register(Game.class);
+//		final Class<? extends DbObject>[] clsa = getClasses();
+//		for (int i = 0; i < clsa.length; i++) {
+//			db.register(clsa[i]);
+//		}
 //		db.init("jdbc:mysql://localhost:3306/testdb", "c", "password", 5);
 //		db.init("jdbc:mysql://localhost:3306/testdb?autoReconnect=true&useSSL=false", "c", "password", 5);
 		db.init("jdbc:mysql://localhost:3306/testdb?useSSL=false", "c", "password", 5);
 //		db.init("jdbc:mysql://localhost:3306/testdb?verifyServerCertificate=false&useSSL=true", "c", "password", 5);
 
 //		Db.log_enable = false;
-		
+
 //		run(fulltext_search_books.class);
 //		run(import_games.class);
 //		run(test.class);
@@ -43,7 +55,7 @@ public class Main {
 //		run(import_books.class);
 //		run(jdbc_select_books.class);
 //		run(get_books.class);
-		
+
 		db.shutdown();
 	}
 }
