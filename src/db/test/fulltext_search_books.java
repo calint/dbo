daed6f854e5ea80ceae514e49df56ffcea6f321d
c <c@c>
2022-11-22 08:13:04 +0100

    .

---
+++ src/db/test/fulltext_search_books.java
@@ -0,0 +1,49 @@
+package db.test;
+
+import java.util.List;
+
+import db.Db;
+import db.DbObject;
+import db.DbTransaction;
+import db.Limit;
+import db.Query;
+
+//// get-book-10req-1M
+public class fulltext_search_books extends TestCase {
+	@Override
+	protected boolean isResetDatabase() {
+		return false;
+	}
+
+	@Override
+	protected boolean isRunWithoutCache() {
+		return false;
+	}
+
+	@Override
+	public void doRun() throws Throwable {
+		final DbTransaction tn = Db.currentTransaction();
+		final int nreq = 1;
+		int i = 0;
+		final String qstr = "+american +idol -shows";
+		final Query qry = new Query(DataText.ft, qstr).and(Book.data);
+		final Limit lmt = new Limit(0, 20);
+		final int totalcount = tn.getCount(Book.class, null);
+		System.out.println("books " + totalcount);
+		while (true) {
+			System.out.println(" searching '" + qstr + "'");
+			final int count = tn.getCount(Book.class, qry);
+			System.out.println("    found " + count);
+			final List<DbObject> ls = tn.get(Book.class, qry, null, lmt);
+			for (final DbObject o : ls) {
+				final Book bo = (Book) o;
+				System.out.println(bo.id() + "  " + bo.getName());
+			}
+			System.out.println("objects retrieved: " + ls.size());
+			i++;
+			System.out.println("requests: " + i);
+			if (i == nreq)
+				break;
+		}
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ import db.test.File;
 import db.test.Game;
 import db.test.TestCase;
 import db.test.User;
+import db.test.fulltext_search_books;
 import db.test.import_books_sample;
 
 public final class Main {
@@ -69,6 +70,7 @@ public final class Main {
 
 //		run(test1.class);
 		run(import_books_sample.class);
+		run(fulltext_search_books.class);
 //		run(import_books.class);
 //		run(jdbc_select_books.class);
 //		run(get_books.class);
