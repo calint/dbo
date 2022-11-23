bb8d46fcf563a4d796434b45286ad995f69059aa
c <c@c>
2022-11-23 03:19:40 +0100

    .

--- src/jem/JavaCodeEmitter.java
+++ src/jem/JavaCodeEmitter.java
@@ -46,45 +46,9 @@ public final class JavaCodeEmitter {
 						.newInstance(dbf);
 				add(jce);
 			} catch (Throwable t) {
-//				throw new RuntimeException("cannot create JavaCodeElem of class '" + elemClsName + "'", t);
-				System.err.println("cannot create JavaCodeElem of class '" + elemClsName + "' for java class " + t.getMessage());
+				System.err.println(
+						"cannot create JavaCodeElem of class '" + elemClsName + "' for java class " + t.getMessage());
 			}
-			continue;
-//			if (dbf instanceof FldString) {
-//				final FldString f = (FldString) dbf;
-//				add(new FldStringElem(f));
-//				continue;
-//			}
-//			if (dbf instanceof FldInt) {
-//				final FldInt f = (FldInt) dbf;
-//				add(new FldIntElem(f));
-//				continue;
-//			}
-//			if (dbf instanceof FldLong) {
-//				final FldLong f = (FldLong) dbf;
-//				add(new FldLongElem(f));
-//				continue;
-//			}
-//			if (dbf instanceof FldFloat) {
-//				final FldFloat f = (FldFloat) dbf;
-//				add(new FldFloatElem(f));
-//				continue;
-//			}
-//			if (dbf instanceof FldDouble) {
-//				final FldDouble f = (FldDouble) dbf;
-//				add(new FldDoubleElem(f));
-//				continue;
-//			}
-//			if (dbf instanceof FldBoolean) {
-//				final FldBoolean f = (FldBoolean) dbf;
-//				add(new FldBooleanElem(f));
-//				continue;
-//			}
-//			if (dbf instanceof FldTimestamp) {
-//				final FldTimestamp f = (FldTimestamp) dbf;
-//				add(new FldTimestampElem(f));
-//				continue;
-//			}
 		}
 		for (final DbRelation dbr : dbc.getDeclaredRelations()) {
 			final StringBuilder sb = new StringBuilder();
@@ -92,30 +56,14 @@ public final class JavaCodeEmitter {
 			sb.append(getClassNameAfterPackageForClass(dbr.getClass())).append("Elem");
 			final String elemClsName = sb.toString();
 
-			final JavaCodeElem jce = (JavaCodeElem) Class.forName(elemClsName).getConstructor(dbr.getClass())
-					.newInstance(dbr);
-			add(jce);
-
-//			if (dbr instanceof RelRef) {
-//				final RelRef r = (RelRef) dbr;
-//				add(new RelRefElem(r));
-//				continue;
-//			}
-//			if (dbr instanceof RelAgg) {
-//				final RelAgg r = (RelAgg) dbr;
-//				add(new RelAggElem(r));
-//				continue;
-//			}
-//			if (dbr instanceof RelRefN) {
-//				final RelRefN r = (RelRefN) dbr;
-//				add(new RelRefNElem(r));
-//				continue;
-//			}
-//			if (dbr instanceof RelAggN) {
-//				final RelAggN r = (RelAggN) dbr;
-//				add(new RelAggNElem(r));
-//				continue;
-//			}
+			try {
+				final JavaCodeElem jce = (JavaCodeElem) Class.forName(elemClsName).getConstructor(dbr.getClass())
+						.newInstance(dbr);
+				add(jce);
+			} catch (Throwable t) {
+				System.err.println(
+						"cannot create JavaCodeElem of class '" + elemClsName + "' for java class " + t.getMessage());
+			}
 		}
 
 		out.println("//****************************************************");
