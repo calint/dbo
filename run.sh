23b6ba55af6120d20fc17766d0f3a121025d048e
calint <calin.tenitchi@gmail.com>
2022-11-27 17:37:49 +0100

    .

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 set -e
 DIR=$(dirname "$0")
 cd $DIR &&
-cmd="java -cp bin`ls $DIR/lib|while read f;do echo -n :;echo -n $DIR/lib/$f;done` main.Main $*" &&
+#cmd="java -cp bin`ls $DIR/lib|while read f;do echo -n :$DIR/lib/$f;done` main.Main $*" &&
+cmd="java -cp bin:lib/mysql-connector-java-5.1.49.jar main.Main $*" &&
 echo \> $cmd;$cmd
